99d8b89692ec9548892da34ce6a4623c2a93e5e4
Edoardo Lincetto <edoardo@aterrae.com>
2017-06-20 16:11:38 +0200

    ADDED: bundling using Rollup.js

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+
+let pkg = require('./package.json');
+
+let plugins = [
+    resolve(),
+    commonjs({
+        include: 'node_modules/**'
+    }),
+    babel({
+        exclude: 'node_modules/**'
+    })
+];
+
+export default {
+    entry: './index.js',
+    plugins: plugins,
+    external: [
+        'ansi-html',
+        'glob',
+        'gulp-util',
+        'handlebars',
+        'through2',
+        'fs',
+        'path'
+    ],
+    globals: {
+        'ansi-html': 'ansiHTML',
+        'glob': 'glob',
+        'gulp-util': 'gutil',
+        'handlebars': 'Handlebars',
+        'through2': 'through',
+        'fs': 'fs',
+        'path': 'path'
+    },
+    targets: [
+        {
+            dest: pkg.main,
+            format: 'cjs',
+            sourceMap: true
+        },
+        {
+            dest: pkg.module,
+            format: 'es',
+            sourceMap: true
+        }
+    ]
+};
